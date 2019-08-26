0dd70e1dfc9b80c99f9d37e40ff0b9c26f65226a
Domingo Gallardo <domingo.gallardo@ua.es>
2019-08-26 08:31:15 +0200

    Añadido mensaje flash

--- src/main/java/madstodolist/controller/LoginController.java
+++ src/main/java/madstodolist/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Map;
 
@@ -18,8 +19,17 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginSubmit(@ModelAttribute LoginData loginData, Model model) {
-        model.addAttribute("mensaje", loginData.geteMail());
-        return "saludo";
+    public String loginSubmit(@ModelAttribute LoginData loginData, Model model, RedirectAttributes flash) {
+
+        // Llamada al servicio para comprobar si el login es correcto
+        Boolean loginCorrecto = loginData.geteMail().equals("domingo@ua") && loginData.getPassword().equals("123");
+
+        if (loginCorrecto) {
+            model.addAttribute("mensaje", "Hola " + loginData.geteMail() + "!!!");
+            return "saludo";
+        }
+        else
+            flash.addFlashAttribute("error", "login incorrecto");
+            return "redirect:/login";
     }
 }
