a80a6b100ce4dc217efb11d24cc65604b0aca29a
Domingo Gallardo <domingo.gallardo@ua.es>
2019-08-26 08:54:46 +0200

    Integrada llamada a servicio y base de datos de prueba

--- src/main/java/madstodolist/controller/LoginController.java
+++ src/main/java/madstodolist/controller/LoginController.java
@@ -1,5 +1,10 @@
 package madstodolist.controller;
 
+import madstodolist.model.Usuario;
+import madstodolist.service.UsuarioService;
+import madstodolist.model.Usuario.LoginStatus;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +17,9 @@ import java.util.Map;
 @Controller
 public class LoginController {
 
+    @Autowired
+    UsuarioService usuarioService;
+
     @GetMapping("/login")
     public String loginForm(Model model) {
         model.addAttribute("loginData", new LoginData());
@@ -22,14 +30,18 @@ public class LoginController {
     public String loginSubmit(@ModelAttribute LoginData loginData, Model model, RedirectAttributes flash) {
 
         // Llamada al servicio para comprobar si el login es correcto
-        Boolean loginCorrecto = loginData.geteMail().equals("domingo@ua") && loginData.getPassword().equals("123");
+        LoginStatus loginStatus = usuarioService.login(loginData.geteMail(), loginData.getPassword());
 
-        if (loginCorrecto) {
+        if (loginStatus == LoginStatus.LOGIN_OK) {
             model.addAttribute("mensaje", "Hola " + loginData.geteMail() + "!!!");
             return "saludo";
-        }
-        else
-            flash.addFlashAttribute("error", "login incorrecto");
+        } else if (loginStatus == LoginStatus.USER_NOT_FOUND) {
+            flash.addFlashAttribute("error", "No existe usuario");
             return "redirect:/login";
+        } else if (loginStatus == LoginStatus.ERROR_PASSWORD) {
+            flash.addFlashAttribute("error", "Contraseña incorrecta");
+            return "redirect:/login";
+        }
+        return "redirect:/login";
     }
 }
