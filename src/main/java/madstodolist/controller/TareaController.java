134a4b37b97e944c69e0807a3b86e0074d7afe1f
Domingo Gallardo <domingo.gallardo@ua.es>
2019-09-13 12:57:29 +0200

    Completado controller con llamada al servicio

--- src/main/java/madstodolist/controller/TareaController.java
+++ src/main/java/madstodolist/controller/TareaController.java
@@ -1,16 +1,16 @@
 package madstodolist.controller;
 
 import madstodolist.controller.exception.UsuarioNotFoundException;
-import madstodolist.model.Tarea;
 import madstodolist.model.Usuario;
+import madstodolist.service.TareaService;
 import madstodolist.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -18,6 +18,9 @@ public class TareaController {
 
     @Autowired
     UsuarioService usuarioService;
+
+    @Autowired
+    TareaService tareaService;
 
     @GetMapping("/usuarios/{id}/tareas/nueva")
     public String formNuevaTarea(@PathVariable(value="id") Long idUsuario, @ModelAttribute TareaData tareaData, Model model) {
@@ -31,7 +34,7 @@ public class TareaController {
 
     @PostMapping("/usuarios/{id}/tareas/nueva")
     public String nuevaTarea(@PathVariable(value="id") Long idUsuario, @ModelAttribute TareaData tareaData, RedirectAttributes flash) {
-        System.out.println("Creada tarea: "+tareaData.getTitulo());
+        tareaService.nuevaTareaUsuario(idUsuario, tareaData.getTitulo());
         return "redirect:/usuarios/"+idUsuario+"/tareas";
     }
 }
